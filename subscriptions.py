96615129e4e573abf0389c40e3bfa9d418ed3eff
nmq-hyt <conoroboyle83@gmail.com>
2020-09-30 21:46:34 +0100

    Refactored several things, project still not done. Still must test code to see if it works as intended, finish up basic commands, get minimum viable product

--- subscriptions.py
+++ subscriptions.py
@@ -1,72 +1,89 @@
 from googleapiclient.discovery import build
 import googleapiclient.errors
 from google_auth_oauthlib.flow import InstalledAppFlow
+from pathlib import path
 import io
-import queue
 import re
-# start a flow, a class which begins the OAuth process, using the locally stored oauth secrets, with a scope to just read/write from my youtube channel
-# user verification is done by redirection to a URL, which provides an access code - the docs vaguely hint that 
-# this method may be taken down in future...
-subs_flow = InstalledAppFlow.from_client_secrets_file("oauth_secrets.json", scopes=["https://www.googleapis.com/auth/youtube"], redirect_uri= "urn:ietf:wg:oauth:2.0:oob")
-# get credentials as described above, this will print a link to console which the user retrieves a validation code 
-creds = subs_flow.run_console()
-## now i build the actual service itself, subs
-# note developer key is left blank here as it depend on each user
-subs = build('youtube', 'v3', developerKey=, credentials = creds)
-##specify that i want a list of my subscriptions, which is accessible if 
-##i authed everything correctly, and do it in chronological order
-#
-##open our target file
-#dump = open('/home/nmq-hyt/projects/python/subs/dummy', 'w+')
+import argparse
+import datetime
+import tempfile
+
+current_timestamp = datetime.date.today()
+
+class channel_record:
+    # A class designed to represent a record of a single channel's video uploads.
+    def update_timestamp(channel_record):
+        channel_record.timestamp = current_timestamp
+
+    def __init__(self, ID, title):
+        self.channel_ID = ID
+        self.channel_title = title
+        self.num_new_vids = 0
+        self.list_of_videos = []
+        self.timestamp = 0
 
 
-def get_subs_as_JSON(target):
+parse_cl = argparse.ArgumentParser()
+parse_cl.add_argument("-scid", help = "Set the channel ID to operate on. Must be set at least once." default = None)
+parse_cl.add_argument("-ssf", help = "Set a target file to write the settings of the tool to.")
+parse_cl.add_argument("-recent", help = "Returns n recent videos from a channel. Requries a valid channel ID.")
+parse_cl.add_argument("-subs", help = "Harvests subscriptions of a user, and turns them into a dictionary value")
+parse_cl.add_argument("-watchlist", help = "Updates a watchlist of channels to watch. Accepts a series of channel IDs as a series of strings")
+args = parse_cl.parse_args()
+
+
+
+def obtain_oauth_permission():
+    # start a flow, a class which begins the OAuth process, using the locally stored oauth secrets, with a scope to just read/write from a youtube channel
+    # user verification is done by redirection to a URL, which provides an access code - the docs vaguely hint that 
+    # this method may be taken down in future...
+    subs_flow = InstalledAppFlow.from_client_secrets_file("oauth_secrets.json", scopes=["https://www.googleapis.com/auth/youtube"], redirect_uri= "urn:ietf:wg:oauth:2.0:oob")
+    # get credentials as described above, this will print a link to console which the user retrieves a validation code 
+    creds = subs_flow.run_console()
+    # now i build the actual service itself, subs
+    subs = build('youtube', 'v3', developerKey=, credentials = creds)
+    return subs
+
+
+
+def get_subs_as_JSON(channel_ID):
     try:
-        assert(isinstance(target,io.TextIOBase) == True)
+        with tempfile.mkdtemp(suffix = "temp_dir", dir = pathlib.Path("~/decentsubs").expanduser() ) as work_dir
+        with tempfile.mkstemp(prefix = "temp_work", dir = work_dir text = True) as workspace
+        # check to make sure the function is receiving a text file when called
+        assert((work_dir.exists() == True) and (workspace.exists() == True)
     except AssertionError:
-        print("Target file is not a file!")
-    except FileNotFoundError:
-        print("File does not exist!")
+        print("Failed to make tempfile!")
 
     # nptoken, or next page token
-    # we use it to  cull the entirety of my subscriptions to harvest to json, by repeatedly calling the api, and changing the nptoken each time
-    #subs_resource is a dict
-    subs_resource_request = subs.subscriptions().list(part = "contentDetails,snippet", mine=True, order="unread", maxResults = 50)
+    # we use it to  cull the entirety of a user's subscriptions to harvest to json, by repeatedly calling the api, and changing the nptoken each time
+    subs_resource_request = subs.subscriptions().list(part = "contentDetails,snippet", channelId = user_channel_ID, order="unread", maxResults = 50)
     subs_resource = subs_resource_request.execute()
     texdump.write(str(subs_resource['items']))
     nptoken = subs_resource['nextPageToken']
-    # we make repeated calls to the api here, cycling through each new page token we're given
-    # to retrieve all 300 ish subscription nodes
     while(('nextPageToken' in subs_resource) == True):
         try:
-            subs_resource_request = subs.subscriptions().list(pageToken = nptoken, part = "contentDetails,snippet", mine=True, order="unread", maxResults = 50)
+            subs_resource_request = subs.subscriptions().list(pageToken = nptoken, part = "contentDetails,snippet",  order="unread", maxResults = 50)
             subs_resource = subs_resource_request.execute()
             texdump.write(str(subs_resource['items']))
             nptoken = subs_resource['nextPageToken']
         except KeyError:
             pass
      # the very last iteration will have no further 'nextPageToken' key:value pair, which will raise a KeyError, so we just pass out of the function
+     # suprisingly this last part took very little time to figure out, but the nptoken loop did
+     # i should stop programming after midnight
 
 
-#Need to consider e and i matching functions
-# makes sense to me: if we  get a true on e or i, we're in the right area, so we should do the seek in the file,
-# and call handle_string
-#
-
-subs = open ("/home/nmq-hyt/projects/python/subs/dummy", 'r')
-text = subs.read(None)
 
 def subs_filter(some_input):
+    # whilst I  thought I had to write an entire parser,
+    #  this function is more like a filter with some ideas i gleaned from reading about formal language theory
+    # i.e. production rules, lookahead, using a stack and regex etc.
 
-    back_stack = []
-    channel_titles = []
-    channel_IDs = []
-    input_length = len(some_inpu)
 
-    for i in range(input_length):
-        back_stack.append(some_input[i])
-   back_stack =  back_stack.reverse()
-    
+
+    # note to self: abstract these routines
+    # inefficent 
     endchar = re.compile(r"," "|" r"}" )
     def match_terminal (i, truth_value = None):
         return lambda truth_value : True if (endchar.match(i)) else False 
@@ -89,7 +106,7 @@ def subs_filter(some_input):
     
     echar = re.compile(r"e")
     def match_echar(i, truth_value= None):
-    return lambda truth_value : True if (resource_start.match[i:i+2]) else False
+    return lambda truth_value : True if (resource_start.match[i]) else False
     
     def increment (x,n):
     return lambda x,n : x + n 
@@ -123,17 +140,23 @@ def subs_filter(some_input):
             "handle_resource" : handle_resource,
             "handle_string_part" : handle_string_part,
             }
+
+    channel_titles = []
+    channel_IDs = []
+    input_length = len(some_input)
+    input_str = str(some_input)
+
         
     for i in range(input_length):
-        if (parsing_table["match_start"](some_input[i]):
+        if (parsing_table["match_start"](some_input[i])):
             increment(i,1)
-            if (parsing_table["match_tchar"](some_input[i]):
+            if (parsing_table["match_tchar"](some_input[i])):
                 increment(i,1)
-                if (parsing_table["match_ichar"](some_input[i]):
+                if (parsing_table["match_ichar"](some_input[i])):
                     parsing_table["handle_title"](some_input[i])
-            elif (parsing_table["match_rchar"](some_input[i]):
+            elif (parsing_table["match_rchar"](some_input[i])):
                 increment(i,1)
-                if (parsing_table["match_echar"](some_input[i]):
+                if (parsing_table["match_echar"](some_input[i])):
                     increment(i,1)
                     parsing_table["handle_resource"](some_input[i])
         else:
@@ -143,10 +166,7 @@ def subs_filter(some_input):
     my_dict = {}
     for i in len(channel_IDs)
         my_dict.update{channel_titles[i]:channel_IDs[i]}
-
+    return my_dict
 
 
     
-get_subs_as_JSON(dump)
-filter_subs(text)
-texdump.close()
